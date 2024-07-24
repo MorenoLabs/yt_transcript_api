a7eebe131fe9d662d67c6e8eafaff0b28c9981b7
morenolabs <dirk.moreno73@gmail.com>
2024-07-24 22:02:57 +0200

    refactor: Update FastAPI endpoint for fetching records from Airtable

--- app_test.py
+++ app_test.py
@@ -2,20 +2,11 @@ from fastapi import FastAPI, HTTPException
 import os
 import logging
 from pyairtable import Table
-from dotenv import load_dotenv
-
-load_dotenv()
 
 app = FastAPI()
 
 # Configure logging
 logging.basicConfig(level=logging.DEBUG)
-
-# Access the environment variables
-# AIRTABLE_TOKEN = os.getenv('AIRTABLE_TOKEN')
-# print(f"AIRTABLE_API_KEY: {AIRTABLE_TOKEN}")
-# AIRTABLE_BASE_ID = os.getenv('AIRTABLE_BASE_ID')
-# AIRTABLE_TABLE_ID= os.getenv('AIRTABLE_TABLE_ID')
 
 @app.get("/")
 async def read_root():
@@ -27,9 +18,6 @@ async def fetch_records():
     AIRTABLE_TOKEN = os.getenv('AIRTABLE_TOKEN')
     AIRTABLE_APP_ID = os.getenv('AIRTABLE_APP_ID')
     AIRTABLE_TABLE_ID = os.getenv('AIRTABLE_TABLE_ID')
-    print(AIRTABLE_TOKEN)
-    print(AIRTABLE_APP_ID)
-    print(AIRTABLE_TABLE_ID)
     try:
         if AIRTABLE_TOKEN and AIRTABLE_APP_ID and AIRTABLE_TABLE_ID:
             logging.debug(AIRTABLE_TOKEN)
@@ -48,3 +36,15 @@ async def fetch_records():
     except Exception as e:
         logging.error(f"An error occurred while fetching records: {e}")
         raise HTTPException(status_code=500, detail="Failed to fetch records from Airtable")
+    
+@app.get("/test")
+async def test():
+    AIRTABLE_TOKEN = os.getenv('AIRTABLE_TOKEN')
+    AIRTABLE_APP_ID = os.getenv('AIRTABLE_APP_ID')
+    AIRTABLE_TABLE_ID = os.getenv('AIRTABLE_TABLE_ID')
+    return {
+                "api_key": AIRTABLE_TOKEN,
+                "base_id": AIRTABLE_APP_ID,
+                "table_name": AIRTABLE_TABLE_ID
+            }
+    

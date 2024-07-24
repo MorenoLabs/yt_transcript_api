970537fde4b4000d734a6b3fc1cf9a4a64486b43
morenolabs <dirk.moreno73@gmail.com>
2024-07-24 09:48:32 +0200

    Update external API URL in test.py

--- test.py
+++ test.py
@@ -2,7 +2,7 @@ import requests
 
 def call_external_api():
     # Call the external API
-    external_api_url = "https://oyster-app-2-4s4pa.ondigitalocean.app/transcribe"
+    external_api_url = "https://oyster-app-2-4s4pa.ondigitalocean.app/"
     external_api_response = requests.post(
         external_api_url,
         json={"video_url": "https://www.youtube.com/watch?v=JLEDwO7JEK4", "api_key": "test"}
